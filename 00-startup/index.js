3ac9761ca9484229e90fa3e8107ae56c98ec432a
magicyangsd <magicyangsd@gmail.com>
2021-07-03 17:25:15 +0800

    first commit

---
+++ 00-startup/index.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!'
+    }
+})
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: '页面加载于' + new Date().toLocaleString()
+    }
+})
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: '学习 JavaScript'},
+            {text: '学习 Vue'},
+            {text: '整个牛项目'}
+        ]
+    }
+})
