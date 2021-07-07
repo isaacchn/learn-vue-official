530dd5ea930cadde9d017d8784ca6465e04c7c2f
Isaac <magicyangsd@gmail.com>
2021-07-07 23:10:50 +0800

    11暂存

--- 11-custom-event/index-11.js
+++ 11-custom-event/index-11.js
@@ -25,3 +25,34 @@ var BaseInput = {
     </label>
     `
 }
+var BaseCheckbox = {
+    model: {
+        prop: 'checked',
+        event: 'change'
+    },
+    props: {
+        checked: Boolean
+    },
+    template: `
+    <input 
+            type="checkbox"
+            v-bind:checked="checked"
+            v-on:change="$emit('change',$event.target.checked)"
+            >
+    `
+}
+var app1 = new Vue({
+    el: '#app-1',
+    components: {
+        'base-input': BaseInput,
+        'base-checkbox': BaseCheckbox
+    },
+    data: {
+        loginVue: true
+    },
+    methods: {
+        change: function (checked) {
+            console.log(checked)//未实现
+        }
+    }
+})
